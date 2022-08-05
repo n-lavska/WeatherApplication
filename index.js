83390d9a11c54b6786b140c6d1a28c8a93db06c7
Natalia <lavskaya@ukr.net>
2022-08-05 23:58:31 +0200

    Added alt to icon image

--- index.js
+++ index.js
@@ -77,13 +77,13 @@ function showTemperature(response) {
   document.querySelector("#feels_like").innerHTML = Math.round(
     response.data.main.feels_like
   );
-  //document.querySelector("#description").innerHTML =
-  // response.data.weather[0].main;
+
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 function search(event) {
   event.preventDefault();
