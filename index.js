f28417d8431114c6468f0f2fceda156762dfc2f8
Natalia <lavskaya@ukr.net>
2022-08-12 22:45:14 +0200

    Added api call for daily forecast

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ let day = days[now.getDay()];
 let dateLine = document.querySelector("#current-time");
 dateLine.innerHTML = `${day} ${hour}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
   let forecastHTML = `<div class="row">`;
@@ -72,6 +73,13 @@ function displayForecast() {
 }
 
 //search engine
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "87c09bdb993a722cfadfd41eadb2c1ec";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function showTemperature(response) {
   console.log(response.data);
   let temperature = Math.round(response.data.main.temp);
@@ -96,6 +104,8 @@ function showTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].main);
+
+  getForecast(response.data.coord);
 }
 function search(event) {
   event.preventDefault();
@@ -140,4 +150,3 @@ function handleSubmit(event) {
   searchCity(city);
 }
 searchCity("Westervoort");
-displayForecast();
